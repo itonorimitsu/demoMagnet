0f4e32e28f1b392d3263ee07153c40f70fb62d4b
itonorimitsu <g1644088@gmail.com>
2019-11-09 20:24:56 +0900

    Initial Commit

---
+++ demoMagnet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demoMagnet
+//
+//  Created by 伊藤永光 on 2019/11/09.
+//  Copyright © 2019 Nito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
